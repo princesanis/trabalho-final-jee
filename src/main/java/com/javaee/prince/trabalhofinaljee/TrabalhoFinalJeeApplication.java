526e4a98abd8b95d166497e8e9fdb17d52c522b3
Prince Sanis <prince.souza@yahoo.com.br>
2019-03-14 23:21:40 +0000

    Criação do projeto

---
+++ src/main/java/com/javaee/prince/trabalhofinaljee/TrabalhoFinalJeeApplication.java
@@ -0,0 +1,13 @@
+package com.javaee.prince.trabalhofinaljee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalJeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalJeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaee/prince/trabalhofinaljee/TrabalhoFinalJeeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javaee.prince.trabalhofinaljee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrabalhoFinalJeeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
